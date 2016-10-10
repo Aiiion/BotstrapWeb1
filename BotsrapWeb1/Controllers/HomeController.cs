f11e43fb89e3ebcb5a774def2423e4bcb35ca1ef
Aiion <alex.soderberg97@gmail.com>
2016-10-10 14:33:28 +0200

    site now working with barely any elements

---
+++ BotsrapWeb1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BotsrapWeb1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: first
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
